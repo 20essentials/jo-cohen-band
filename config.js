94ea827197bc7ebefc77ad381587e5d7331cf22d
DominicDev <s0l0estudi4r@gmail.com>
2025-10-25 12:29:51 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' JO COHEN';
+export let nameSongs = [
+	"WE ARE", /*1*/
+	"GLOWING AT NIGHT", /*2*/
+	"LOVE IS GONE", /*3*/
+	"HIGH", /*4*/
+	"DON'T LOOK BACK", /*5*/
+	"ON MY MIND", /*6*/
+	"SYMPATHY", /*7*/
+	"RUN AWAY", /*8*/
+	"WHERE WAS I", /*9*/
+	"ONE SIDED", /*10*/
+	"ONE STEP", /*11*/
+	"TIME BOMB", /*12*/
+	"THE ONE", /*13*/
+	"REBOUND", /*14*/
+	"#BOSS GYAL", /*15*/
+	"BORN TO BE A KING", /*16*/
+	"UPSIDE DOWN", /*17*/
+	"VILLAIN", /*18*/
+	"LUNE", /*19*/
+	"INFINITE", /*20*/
+]
